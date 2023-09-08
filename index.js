2c3befad8b9b135adde4853c0b7d6e385fe6ba88
Aydan Murshud <aydinmurshud@gmail.com>
2023-09-08 13:56:48 +0300

    fix: service section not showing

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ import { autoSlide, createServicesList, createSlideShow, elGen } from "./utils.j
 const slides = document.getElementsByClassName('slide');
 const carouselContainer = document.getElementById('carousel-container');
 const servicesContainer = document.getElementById('services-container');
-createSlideShow(carouselContainer, data)
-createServicesList(servicesContainer, servicesData)
+
+createSlideShow(carouselContainer, data);
+createServicesList(servicesContainer, servicesData);
+
 let activeIndex = 0;
 setInterval(() => {
     if (activeIndex < slides.length - 1) {
